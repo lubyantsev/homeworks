d1b06339729c852472bf9c82c594b0c2490a7a6c
lubyantsev <162503570+lubyantsev@users.noreply.github.com>
2024-08-30 10:41:48 +0300

    module_13_2.py

--- module_13_2.py
+++ module_13_2.py
@@ -3,25 +3,25 @@ from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.utils import executor
 
-API_TOKEN = ''  # Замените на ваш токен
+API_TOKEN = 'YOUR_API_TOKEN_HERE'  # Замените на ваш токен
 
 # Настройка логирования
 logging.basicConfig(level=logging.INFO)
 
-# Создание объектов бота и диспетчера
-bot = Bot(token=
+# Инициализация бота и диспетчера
+bot = Bot(token=API_TOKEN)
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
 
+# Обработчик команды /start
 @dp.message_handler(commands=['start'])
 async def start(message: types.Message):
-    print("Привет! Я бот помогающий твоему здоровью.")
-    await message.reply("Привет! Я бот помогающий твоему здоровью.")
+    print('Привет! Я бот помогающий твоему здоровью.')
 
-@dp.message_handler(lambda message: True)  # Обработчик для всех остальных сообщений
+# Обработчик для всех других сообщений
+@dp.message_handler()
 async def all_messages(message: types.Message):
-    print("Введите команду /start, чтобы начать общение.")
-    await message.reply("Введите команду /start, чтобы начать общение.")
+    print('Введите команду /start, чтобы начать общение.')
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
