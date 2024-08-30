14182ddeb2fc96b46b4be9f6e5aa8e221dded419
lubyantsev <162503570+lubyantsev@users.noreply.github.com>
2024-08-30 10:21:38 +0300

    module_13_2.py

---
+++ module_13_2.py
@@ -0,0 +1,27 @@
+import logging
+from aiogram import Bot, Dispatcher, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.utils import executor
+
+API_TOKEN = '7496069476:AAEmEJvx70OUkElm_JV8q4BypnswN3Nk0aA'  # Замените на ваш токен
+
+# Настройка логирования
+logging.basicConfig(level=logging.INFO)
+
+# Создание объектов бота и диспетчера
+bot = Bot(token=7496069476:AAEmEJvx70OUkElm_JV8q4BypnswN3Nk0aA
+storage = MemoryStorage()
+dp = Dispatcher(bot, storage=storage)
+
+@dp.message_handler(commands=['start'])
+async def start(message: types.Message):
+    print("Привет! Я бот помогающий твоему здоровью.")
+    await message.reply("Привет! Я бот помогающий твоему здоровью.")
+
+@dp.message_handler(lambda message: True)  # Обработчик для всех остальных сообщений
+async def all_messages(message: types.Message):
+    print("Введите команду /start, чтобы начать общение.")
+    await message.reply("Введите команду /start, чтобы начать общение.")
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
